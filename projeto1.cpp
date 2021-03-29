95fdeee3056c43e805de1bad3cb6f8d04bd8fb48
root <root@LAPTOP-IR04B4L1.localdomain>
2021-03-29 18:41:06 +0100

    A meio

--- projeto1.cpp
+++ projeto1.cpp
@@ -5,11 +5,62 @@
 
 #include <iostream>
 #include <vector>
-#include <iterator>
+#include <list>
+#include <stack>
 
 using namespace std;
 
-void processInput(int numberNodes, int numberEdges, vector<vector<int>>& edges1, vector<vector<int>>& edges2){
+class Graph{
+    int V;
+    list <int> *adj;
+public:
+    Graph(int V);
+    void addEdge(int v, int w);
+    int DFS(int s, int counter);
+};
+
+Graph::Graph(int V)
+{
+    this->V = V;
+    adj = new list<int>[V];
+}
+
+void Graph::addEdge(int v, int w)
+{
+    adj[v].push_back(w);
+}
+
+
+int Graph::DFS(int s, int counter){
+
+    vector<bool> visited(V, false);
+    stack<int> stack;
+    stack.push(s);
+    int aux = 0;
+
+    while (!stack.empty()){
+        s = stack.top();
+        stack.pop();
+        aux ++;
+
+        if (!visited[s])
+            visited[s] = true;
+
+        for (auto i = adj[s].begin(); i != adj[s].end(); ++i){
+            if (!visited[*i])
+                stack.push(*i);
+        }
+
+        if (adj[s].empty()){
+            if (aux > counter)
+                counter = aux;
+            aux--;
+        }
+    }
+    return counter;
+}
+
+void processInput(Graph g, int numberNodes, int numberEdges, vector<vector<int>>& edges1, vector<vector<int>>& edges2){
 
     int a, b;
 
@@ -22,7 +73,7 @@ void processInput(int numberNodes, int numberEdges, vector<vector<int>>& edges1,
     }
     for(int i = 0; i < numberEdges; i++){
         scanf("%d %d", &a, &b);
-        
+        g.addEdge(a - 1, b - 1);
         edges1[a-1].push_back(b);
         edges2[b-1].push_back(a);
     }
@@ -40,8 +91,18 @@ int FindFirstOutput(vector< vector<int>>& edges2, vector<int>& InicialNodes){
     return counter;
 }
 
-int FindSecondOutput(vector<vector<int>>& edges2, vector<int>& InicialNodes){
-    /*FazerDFS 2 vezes*/
+int FindSecondOutput(Graph g, vector<vector<int>>& edges2, vector<int>& InicialNodes, int numberNodes){
+    int i, counter = 0, largest = 0;
+    for (i = 0; i < numberNodes; i++){
+        if (edges2[i].empty()){
+            counter = g.DFS(i, counter);
+        }
+        if (counter > largest){
+            largest = counter;
+        }
+        counter = 0;
+    }
+
 }
 
 int main(){
@@ -51,12 +112,12 @@ int main(){
     vector<int> InicialNodes;
 
     scanf("%d %d", &numberNodes, &numberEdges);
-
-    processInput(numberNodes, numberEdges, edges1, edges2);
+    Graph g(numberNodes);
+    processInput(g, numberNodes, numberEdges, edges1, edges2);
 
     firstOutput = FindFirstOutput(edges2, InicialNodes);
 
-    secondOutput = FindSecondOutput(edges1, InicialNodes);
+    secondOutput = FindSecondOutput(g, edges1, InicialNodes, numberNodes);
 
     cout << firstOutput<< " " << secondOutput << endl;
 
