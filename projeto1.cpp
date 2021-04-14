9455751e8534bcc6974b0a9b5dfd15f4ddaa1010
root <root@LAPTOP-IR04B4L1.localdomain>
2021-04-14 12:27:36 +0100

    Fim com relatório

--- projeto1.cpp
+++ projeto1.cpp
@@ -1,8 +1,12 @@
+/* 1st ASA Project
+   Group 50
+   Bernardo Castiço ist196845
+   Hugo Rita ist 196870
+   */
+
 #include <iostream>
 #include <vector>
-#include <list>
 #include <stack>
-#include <algorithm>
 
 #define WHITE 0
 #define GREY 1
@@ -26,7 +30,7 @@ int Graph::DFS(int w, vector<vector<int>>& edges1){
 
     vector<int> father(V, -1);
     vector<int> color(V, WHITE);
-    vector<int> nodesDown(V, 1);
+    vector<int> nodesDown(V, 1); /* Number of nodes that a node can put down */
     stack<int> stack;
     stack.push(w);
     int s;
@@ -39,13 +43,13 @@ int Graph::DFS(int w, vector<vector<int>>& edges1){
             color[s] = GREY;
 
             for(int j = 0; j < int(edges1[s].size()); j++){
-                stack.push(edges1[s][j]);
+                stack.push(edges1[s][j]); /* Put the adjacents of 's' in the stack */
                 father[edges1[s][j]] = s;
             }
         }
         else if(color[s] == GREY){
             color[s] = BLACK;
-            if(father[s] != -1)
+            if(father[s] != -1) /* Verify if 's' has an antecessor and if it does updates (or not) his nodes down */
                 if(nodesDown[father[s]] < 1 + nodesDown[s])
                     nodesDown[father[s]] = 1 + nodesDown[s];
 
@@ -60,6 +64,7 @@ int Graph::DFS(int w, vector<vector<int>>& edges1){
         }
 
     }
+
     return nodesDown[w];
 }
 
@@ -71,33 +76,37 @@ void processInput(Graph g, int numberNodes, int numberEdges, vector<vector<int>>
     edges2.reserve(numberEdges);
 
     for(int i = 0; i < numberNodes; i++){
-        edges1.insert(edges1.begin() + i, vector<int>());
+        edges1.insert(edges1.begin() + i, vector<int>()); 
         edges2.insert(edges2.begin() + i, vector<int>());
     }
+
     for(int i = 0; i < numberEdges; i++){
         if(!scanf("%d %d", &a, &b)){
-			cout << "Erro" << endl;
+			cout << "Error" << endl;
 		}
-        edges1[a-1].push_back(b-1);
-        edges2[b-1].push_back(a);
-}
+        edges1[a-1].push_back(b-1); /* Create the adjacencies list of a node */
+        edges2[b-1].push_back(a);   /* Create the antecessors list of a node */
+    }
 }
 
 int FindFirstOutput(vector< vector<int>>& edges2, vector<int>& InicialNodes){
     int counter = 0, i;
+
     for(i = 0; i < int(edges2.size()); i++){
         if(edges2[i].empty()){
-            InicialNodes.push_back(i+1);
+            InicialNodes.push_back(i+1); /* Create a nodes list without antecessors */
             counter++;
         }
     }
+
     return counter;
 }
 
 int FindSecondOutput(Graph g,vector<vector<int>>& edges2, vector<vector<int>>& edges1, vector<int>& InicialNodes, int numberNodes){
     int i, counter = 1, largest = 0;
+
     for (i = 0; i < numberNodes; i++){
-        if (edges2[i].empty() && !edges1[i].empty()){
+        if (edges2[i].empty() && !edges1[i].empty()){ /* Only call DFS for the nodes without antecessors */
             counter = g.DFS(i, edges1);
         }
         if (counter > largest){
@@ -105,6 +114,7 @@ int FindSecondOutput(Graph g,vector<vector<int>>& edges2, vector<vector<int>>& e
         }
         counter = 1;
     }
+
     return largest;
 }
 
@@ -115,7 +125,7 @@ int main(){
     vector<int> InicialNodes;
 
     if(!scanf("%d %d", &numberNodes, &numberEdges)){
-        cout << "Erro" << endl;
+        cout << "Error" << endl;
 
     }
 
